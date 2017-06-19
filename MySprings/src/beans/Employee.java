063f7cbbf69cebd034247e081a731696409eb206
anilchowdary257 <anilchowdary257@gmail.com>
2017-06-19 21:55:55 +0530

    My First Java Spring program

---
+++ MySprings/src/beans/Employee.java
@@ -0,0 +1,16 @@
+package beans;
+
+public class Employee {
+
+	private String ename;
+	private String eId;
+	
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	public void seteId(String eId) {
+		this.eId = eId;
+	}
+	
+}
---
+++ MySprings/src/beans/MyClinet.java
@@ -0,0 +1,10 @@
+package beans;
+
+public class MyClinet {
+
+	public static void main(String[] args) {
+		
+		System.out.println("first program");
+	}
+
+}
